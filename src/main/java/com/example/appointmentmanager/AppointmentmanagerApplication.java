6e742ddfe6e4ac69f0154850df03703aacca6dca
Eloundou Victor <mvogoeloundou@gmail.com>
2026-05-23 11:49:25 +0100

    Commit initial

---
+++ src/main/java/com/example/appointmentmanager/AppointmentmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appointmentmanager/AppointmentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
